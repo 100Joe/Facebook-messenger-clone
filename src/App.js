d46a52c5067a3b35c2b382526dcd9809a3665327
Joseph Edmonds <consultingjde@gmail.com>
2021-07-26 15:11:12 -0700

    Styling with Material-ui and creating Message component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  // useState = Variable in React that can be changed without refreshing. 
+  // useEffect = run code on a condition in React
+
+  useEffect(() => {
+    // Run code here
+    // const username = prompt('Please enter your name')
+    setUsername(prompt('Please enter your name'));
+  }, []) //Condition  if it is blank inside [], this code runs Once when the app component loads. If there is a variable it will run every time the variable changes.
+
+  const sendMessage = (event) => {
+    // Messaging Logic
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome {username}</h1>
+      {/* Input field */}
+      <form>
+        <FormControl>
+          <InputLabel >Enter a message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+
+          {/* Button */}
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
+
+      {/* Display Messages */}
+      {
+        messages.map(message => (
+          <Message text={message} />
+        ))
+      }
     </div>
   );
 }
